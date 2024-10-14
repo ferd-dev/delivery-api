cd357f6d00499fffd198ca549fd0aaeceab5b18d
Fernando <vluisfernando62@gmail.com>
2024-10-14 12:25:26 -0400

    config: create project and initialize repository

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function () {
+    return ['message' => 'Hello World!'];
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
