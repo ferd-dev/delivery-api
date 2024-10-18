9a73b943255a828f19a6749f5ca2a761d0647859
Fernando <vluisfernando62@gmail.com>
2024-10-18 15:36:13 -0400

    feat(Establisment): add the route to see an establishment

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::post('/login', [AuthenticationController::class, 'login']);
 Route::middleware(('auth:sanctum'))->group(function () {
 
     Route::get('/establisments', [EstablishmentController::class, 'index']);
+    Route::get('/establisments/{establishment}', [EstablishmentController::class, 'show']);
 
     Route::get('/user', function (Request $request) {
         return $request->user();
