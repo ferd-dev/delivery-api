02d7d1f502e8382c681cd69df73de3d9d63a16f0
Fernando <vluisfernando62@gmail.com>
2024-10-18 15:31:05 -0400

    fix(Establishment): add authorization to access methods

--- app/Http/Controllers/EstablishmentController.php
+++ app/Http/Controllers/EstablishmentController.php
@@ -10,6 +10,8 @@ class EstablishmentController extends Controller
 {
     public function index()
     {
+        abort_unless(Auth::user()->tokenCan('establishment:show'), 403, "You don't have permission to view this establishment");
+
         return Establishment::when(request()->filled('category'), function ($query) {
             $query->where('category', request('category'));
         })
@@ -21,6 +23,8 @@ class EstablishmentController extends Controller
 
     public function show(Establishment $establishment)
     {
+        abort_unless(Auth::user()->tokenCan('establishment:show'), 403, "You don't have permission to view this establishment");
+
         return $establishment;
     }
 }
