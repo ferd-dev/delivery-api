bd28c847da4b0f12f65a4a2c2bcbaead5ed47aef
Fernando <vluisfernando62@gmail.com>
2024-10-18 14:40:10 -0400

    feat(api routes): group routes that require authentication

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\EstablishmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthenticationController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(('auth:sanctum'))->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
