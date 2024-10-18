6c12567f5623ecd5f8efa45971d45dfc15cea471
Fernando <vluisfernando62@gmail.com>
2024-10-18 15:38:19 -0400

    feat(Establishment): add a EstablishmentResource file to display the necessary data for an establishment

--- app/Http/Controllers/EstablishmentController.php
+++ app/Http/Controllers/EstablishmentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\EstablishmentResource;
 use App\Models\Establishment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,19 +13,21 @@ class EstablishmentController extends Controller
     {
         abort_unless(Auth::user()->tokenCan('establishment:show'), 403, "You don't have permission to view this establishment");
 
-        return Establishment::when(request()->filled('category'), function ($query) {
+        $establishment = Establishment::when(request()->filled('category'), function ($query) {
             $query->where('category', request('category'));
         })
             ->when(request()->exists('popular'), function ($query) {
                 $query->orderBy('starts', 'DESC');
             })
             ->paginate(10);
+
+        return EstablishmentResource::collection($establishment);
     }
 
     public function show(Establishment $establishment)
     {
         abort_unless(Auth::user()->tokenCan('establishment:show'), 403, "You don't have permission to view this establishment");
 
-        return $establishment;
+        return new EstablishmentResource($establishment);
     }
 }
