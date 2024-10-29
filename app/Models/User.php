12631bebfd571a3886705d0c40689b31a08c024d
Fernando <vluisfernando62@gmail.com>
2024-10-29 19:16:04 -0400

    feat(User): add the two methods to verify if it is a client or delivery

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function isClient()
+    {
+        return $this->role === 'client';
+    }
+    public function isDelivery()
+    {
+        return $this->role === 'delivery';
+    }
 }
